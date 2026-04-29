747731db8ab729bf3bb54eafd39aafb62c7eebca
Valerio Hrdo <86080576+Hrdo0@users.noreply.github.com>
2026-04-29 21:25:06 +0200

    h

--- Script/ShitTrackerMain.py
+++ Script/ShitTrackerMain.py
@@ -10,6 +10,7 @@ from Create_Voice import upload_to_github_api,get_comment_from_gemini,parla_pro
 
 sub_prompt = '''
 La seguente è una classifica cacatoria, ogni numero corrisponde al numero di cacate che ha fatto una persona nell'arco di un mese,commentala come se fossi un telecronista sportivo particolarmente scurrile e colorito, usa un sacco di parolacce ed eufemismi relativi al gabinetto. 
+Attenzione perchè questo mese è molto particolare, questo mese la vittoria non se la aggiudicherà colui il quale avrà fatto piu cacate bensi il criterio rimarrà segreto fino a domani
 Il testo che mi restituirai verrà dato in pasto ad un text-to-speech quindi non usare emoji e non essere particolarmente prolisso, non usare termini inglesi e non usare simboli come "*" o "#":\n
 '''
 
@@ -18,7 +19,7 @@ UPLOAD_GIT_FLAG = True
 if __name__ == "__main__":
     # DATETIME.DATETIME(ANNO , MESE, GIORNO)
     data_inizio  = datetime.datetime(year = 2026, month = 4, day = 1)
-    data_fine =  datetime.datetime(year = 2026, month = 4, day = 20)# datetime.datetime.now()
+    data_fine =  datetime.datetime(year = 2026, month = 4, day = 29)# datetime.datetime.now()
     print("Inizializzazione Chat...")
     try:
         chat = Chat("Script/Chat WhatsApp con cacatorio.txt")
